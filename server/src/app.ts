3d96fea44af1f9255f3ba9de8c6f3bd2c463bd6b
emredalkiran <emre59dalkiran@gmail.com>
2021-02-17 15:20:28 +0300

    App and server uses new service container object

---
+++ server/src/app.ts
@@ -0,0 +1,71 @@
+import express from 'express'
+import expressWinston from 'express-winston'
+import helmet from 'helmet'
+import winston from 'winston'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import session from 'express-session'
+import { ServiceContainer } from './service-container'
+import { v4 as uuidv4 } from 'uuid'
+import redis from 'redis'
+import connectRedis from 'connect-redis'
+import { UserRouter } from './user/user-router'
+import { SessionOptions } from 'express-session'
+
+export class App {
+  instance: express.Application
+
+  constructor() {
+    this.instance = express()
+  }
+
+  private setRoutes(serviceContainer: ServiceContainer): void {
+    const userRouter = new UserRouter(this.instance, serviceContainer)
+  }
+
+  configureApp(serviceContainer: ServiceContainer): void {
+    const RedisStore = connectRedis(session)
+    const redisClient = redis.createClient()
+
+    const sessionOptions: SessionOptions = {
+      genid: () => uuidv4(),
+      store: new RedisStore({ client: redisClient }),
+      secret: process.env.SESSION_SECRET!,
+      resave: false,
+      cookie: {
+        secure: process.env.NODE_ENV === 'development' ? false : true,
+        sameSite: true,
+        httpOnly: true,
+        maxAge: 3600000
+      },
+      saveUninitialized: false
+    }
+
+    this.instance.use(helmet())
+    this.instance.use(bodyParser.urlencoded({ extended: true }))
+    this.instance.use(bodyParser.json())
+    this.instance.use(cors<express.Request>())
+    this.instance.use(session(sessionOptions))
+    this.instance.use(
+      expressWinston.logger({
+        transports: [new winston.transports.Console()],
+        format: winston.format.combine(
+          winston.format.colorize(),
+          winston.format.json()
+        )
+      })
+    )
+    this.setRoutes(serviceContainer)
+    this.instance.use(
+      expressWinston.errorLogger({
+        transports: [new winston.transports.Console()],
+        format: winston.format.combine(
+          winston.format.colorize(),
+          winston.format.json()
+        )
+      })
+    )
+  }
+}
+
+export default App
---
+++ server/src/server.ts
@@ -0,0 +1,31 @@
+import env from 'dotenv'
+import http from 'http'
+import MongoConnection from './utils/database'
+import App from './app'
+import { creaeteServiceContainer } from './service-container'
+
+async function init() {
+  env.config()
+  const connectionURL = `mongodb://${process.env.URL}`
+  const port = process.env.PORT || 3000
+  const databaseConnection = new MongoConnection()
+  const app = new App()
+  try {
+    const db = await databaseConnection.connect(
+      connectionURL,
+      process.env.DATABASE_NAME!
+    )
+    console.log(`Connected to ${process.env.DATABASE_NAME} database `)
+    console.log('Starting server...')
+    const serviceContainer = creaeteServiceContainer(db)
+    app.configureApp(serviceContainer)
+    const server = http.createServer(app.instance)
+    server.listen({ host: 'localhost', port: port }, () =>
+      console.log(`Server listening on port ${port} at localhost`)
+    )
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+init()
