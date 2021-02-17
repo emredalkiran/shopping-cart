0f42ca091ed853d608b527e55f80f31c1adef699
emredalkiran <emre59dalkiran@gmail.com>
2021-02-17 15:26:05 +0300

    Initial commit

---
+++ server/src/utils/hash.ts
@@ -0,0 +1,16 @@
+import bcrypt from 'bcrypt'
+
+export interface Hash {
+  hash(password: string): Promise<string>
+  validate(password: string, hashedPassword: string): Promise<boolean>
+}
+
+export default class HashHelper implements Hash {
+  hash(password: string): Promise<string> {
+    const saltRound = 10
+    return bcrypt.hash(password, saltRound)
+  }
+  validate(password: string, hashedPassword: string): Promise<boolean> {
+    return bcrypt.compare(password, hashedPassword)
+  }
+}
---
+++ server/src/utils/interfaces.ts
@@ -0,0 +1,20 @@
+import { Session } from 'express-session'
+
+export interface User {
+  name: string
+  lastName: string
+  email: string
+  hashedPassword: string
+}
+
+export interface UserAuthenticationRequest {
+  email: string
+  password: string
+}
+
+export interface RequestData {
+  body: object
+  queryParams: object
+  sessionId?: string
+  session: Session
+}
