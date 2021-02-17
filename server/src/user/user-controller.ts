842718ed166f5e3a1d759e4b5f7ab8418c317910
emredalkiran <emre59dalkiran@gmail.com>
2021-02-17 14:49:16 +0300

    Added tsconfig

---
+++ server/src/user/user-controller.ts
@@ -0,0 +1,47 @@
+import express from 'express'
+import BaseController from '../base-classes/base-controller'
+import { httpHeader, statusCode } from '../utils/http-header'
+import { UserServiceInterface } from './user-service'
+
+export default class UserController extends BaseController {
+  userService: UserServiceInterface
+  constructor(userService: UserServiceInterface) {
+    super()
+    this.userService = userService
+  }
+  async login(req: express.Request, res: express.Response): Promise<void> {
+    const reqData = this.getRequestData(req)
+    try {
+      const response = await this.userService.login(reqData)
+      res.set(httpHeader.json).status(statusCode.success).send(response)
+    } catch (err) {
+      res
+        .set(httpHeader.json)
+        .status(statusCode.unauthorized)
+        .send({
+          response: {
+            success: false,
+            error: err.errorMessage
+          }
+        })
+    }
+  }
+
+  async signup(req: express.Request, res: express.Response): Promise<void> {
+    const reqData = this.getRequestData(req)
+    try {
+      const response = await this.userService.addUser(reqData)
+      res.set(httpHeader.json).status(statusCode.success).send(response)
+    } catch (err) {
+      res
+        .set(httpHeader.json)
+        .status(statusCode.badRequest)
+        .send({
+          response: {
+            success: false,
+            error: err.errorMessage
+          }
+        })
+    }
+  }
+}
