261b8890f5affea6338376075ad55661329f0009
emredalkiran <emre59dalkiran@gmail.com>
2021-02-20 01:12:06 +0300

    Restructured to include main components

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,15 +1,82 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, {
+  MouseEvent,
+  TouchEvent,
+  useState,
+  useEffect,
+  useCallback
+} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Navbar from '../Navbar'
+import Content from '../Content'
+import Modal from '../Modal'
+import ShoppingItem from '../ShoppingItem'
+import LoginForm from '../Forms/LoginForm'
+import SignupForm from '../Forms/SignupForm'
+import ItemDetail from '../ItemDetail'
+import {
+  getProducts,
+  selectProducts
+} from '../../features/product/product-slice'
+import './main.css'
 
 function App() {
+  const [modalType, setModalType] = useState<string>('')
+  const [productID, setProductId] = useState<string>()
+  const [showCloseButton, setShowCloseButton] = useState<boolean>(false)
+  const [isModalOpen, setModalOpenStatus] = useState<boolean>(false)
+  const dispatch = useDispatch()
+  const items = useSelector(selectProducts)
+  const handleClick = (e: MouseEvent | TouchEvent) => {
+    setModalOpenStatus(true)
+    setModalType(e.currentTarget.getAttribute('data-name')!)
+    if (e.currentTarget.getAttribute('data-name') === 'item') {
+      setShowCloseButton(true)
+      setProductId(e.currentTarget.getAttribute('data-id')!)
+    }
+  }
+
+  const handleClose = () => {
+    setModalOpenStatus(false)
+    setShowCloseButton(false)
+    setModalType('')
+  }
+  const getProductsData = useCallback(async () => {
+    await dispatch(getProducts())
+  }, [dispatch])
+
+  useEffect(() => {
+    getProductsData()
+  }, [getProductsData])
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. Learn React
-        </p>
-      </header>
+    <div className='main'>
+      <Navbar handleClick={handleClick} />
+      <Content>
+        {items?.map(({ productId, name, stock, price }, index) => {
+          return (
+            <ShoppingItem
+              key={`${index}-${name}`}
+              id={productId}
+              index={index + 1}
+              name={name}
+              quantity={stock}
+              price={price}
+              handleClick={handleClick}
+            />
+          )
+        })}
+      </Content>
+      <Modal
+        open={isModalOpen}
+        close={handleClose}
+        withCloseButton={showCloseButton}
+      >
+        {modalType === 'login' && <LoginForm />}
+        {modalType === 'signup' && <SignupForm />}
+        {modalType === 'item' && (
+          <ItemDetail {...items?.find((el) => el.productId === productID)!} />
+        )}
+      </Modal>
     </div>
   )
 }
