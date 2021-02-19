879a6412c4a887f5182e5d4b8af36b3ebe314e73
emredalkiran <emre59dalkiran@gmail.com>
2021-02-20 01:23:23 +0300

    State types for slices aded

--- client/src/features/types/index.ts
+++ client/src/features/types/index.ts
@@ -1,14 +1,23 @@
+interface Product {
+  name: string
+  productId: string
+  price: number
+  stock: number
+}
+
 export interface AuthState {
   isLoggedIn: boolean
   name: string
   authError: string
 }
 
-export interface ShopState {
-  itemsInCart: number
+export interface ProductState {
+  products: Array<Product>
+  shoppingCart: Array<string>
+  isProductsLoading: boolean
 }
 
 export interface RootState {
   auth: AuthState
-  shop: ShopState
+  product: ProductState
 }
