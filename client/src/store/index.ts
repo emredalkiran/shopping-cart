61e29a21864bca85504e17a6f14702b7cff592ce
emredalkiran <emre59dalkiran@gmail.com>
2021-02-20 01:23:42 +0300

    Added product slice to store

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import authReducer from '../features/auth/authSlice'
-
+import authReducer from '../features/auth/auth-slice'
+import productReducer from '../features/product/product-slice'
 const store = configureStore({
   reducer: {
-    auth: authReducer
+    auth: authReducer,
+    product: productReducer
   }
 })
 
