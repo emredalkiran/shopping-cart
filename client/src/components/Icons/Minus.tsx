c4cf29ef6568f4d9316bc2efa2ac749f968f8ba7
emredalkiran <emre59dalkiran@gmail.com>
2021-02-23 15:35:27 +0300

    Convert icons to tsx extension

--- client/src/components/Icons/Minus.tsx
+++ client/src/components/Icons/Minus.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from 'react'
 
 function SvgMinus(props) {
   return (
@@ -11,7 +11,7 @@ function SvgMinus(props) {
     >
       <path d="M392.533 187.733H17.067C7.641 187.733 0 195.374 0 204.8s7.641 17.067 17.067 17.067h375.467c9.426 0 17.067-7.641 17.067-17.067s-7.642-17.067-17.068-17.067z" />
     </svg>
-  );
+  )
 }
 
-export default SvgMinus;
+export default SvgMinus
