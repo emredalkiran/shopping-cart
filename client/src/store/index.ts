66a97567192264e7fc8e6573349b5f799094664e
emredalkiran <emre59dalkiran@gmail.com>
2021-02-23 15:36:56 +0300

    Used custom dispatch for correct return types

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { useDispatch } from 'react-redux'
 import authReducer from '../features/auth/auth-slice'
 import productReducer from '../features/product/product-slice'
 const store = configureStore({
@@ -9,3 +10,6 @@ const store = configureStore({
 })
 
 export default store
+
+export type AppDispatch = typeof store.dispatch
+export const useAppDispatch = () => useDispatch<AppDispatch>()
