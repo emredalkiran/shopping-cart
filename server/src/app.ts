21474f4eca422f5b5d67a049c827e14cc02b9331
emredalkiran <emre59dalkiran@gmail.com>
2021-02-25 23:17:04 +0300

    Socket + static file path updates

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,7 +3,6 @@ import expressWinston from 'express-winston'
 import helmet from 'helmet'
 import winston from 'winston'
 import bodyParser from 'body-parser'
-import cors from 'cors'
 import session from 'express-session'
 import { ServiceContainer } from './service-container'
 import { v4 as uuidv4 } from 'uuid'
@@ -13,10 +12,12 @@ import { UserRouter } from './user/user-router'
 import { ProductRouter } from './product/product-router'
 import { SessionOptions } from 'express-session'
 import { CartRouter } from './cart/cart-router'
+import { Server } from 'socket.io'
+import path from 'path'
+import { ViewRouter } from './view/view-router'
 
 export class App {
   instance: express.Application
-
   constructor() {
     this.instance = express()
   }
@@ -25,40 +26,67 @@ export class App {
     const userRouter = new UserRouter(this.instance, serviceContainer)
     const productRouter = new ProductRouter(this.instance, serviceContainer)
     const cartRouter = new CartRouter(this.instance, serviceContainer)
+    const viewRouter = new ViewRouter(this.instance)
   }
 
-  configureApp(serviceContainer: ServiceContainer): void {
+  configureApp(serviceContainer: ServiceContainer, io: Server): void {
     const RedisStore = connectRedis(session)
-    const redisClient = redis.createClient()
-    const sessionStore = new RedisStore({ client: redisClient, ttl: 1800 })
-
+    const redisClient = redis.createClient('redis://redis:6379')
+    const sessionStore = new RedisStore({
+      client: redisClient,
+      ttl: 1800
+    })
     const sessionOptions: SessionOptions = {
       genid: () => uuidv4(),
       store: sessionStore,
       secret: process.env.SESSION_SECRET!,
       resave: false,
       cookie: {
-        secure: false,
-        httpOnly: false,
-        maxAge: 1800000
+        secure: false, //should set to true on production environment
+        httpOnly: true,
+        maxAge: 1800000,
+        sameSite: true
       },
       saveUninitialized: false
     }
-
+    const sessionMiddleware = session(sessionOptions)
     this.instance.use(helmet())
     this.instance.use(bodyParser.urlencoded({ extended: true }))
     this.instance.use(bodyParser.json())
+    this.instance.use(sessionMiddleware)
     this.instance.use(
-      cors<express.Request>({
-        origin: [
-          'http://127.0.0.1:3000',
-          'http://127.0.0.1:3000/',
-          'http://127.0.0.1:8080'
-        ],
-        credentials: true
-      })
+      express.static(path.join(__dirname, '../../client/client-build'))
     )
-    this.instance.use(session(sessionOptions))
+
+    io.use((socket, next) => {
+      //@ts-ignore
+      socket.request.originalUrl = socket.request.url
+      //@ts-ignore
+      sessionMiddleware(socket.request, {}, next)
+    })
+
+    io.on('connection', (socket) => {
+      const userId = socket.request.session.userId
+        ? socket.request.session.userId
+        : null
+
+      if (!userId) socket.disconnect(true)
+      else {
+        socket.join(userId)
+        console.log(userId, 'joined')
+        socket.on('addItemToCart', (msg: Record<string, string | number>) => {
+          console.log('addItemToCart sent ', msg)
+          socket.to(userId).emit('addItemToCart', msg)
+        })
+        socket.on(
+          'removeItemFromCart',
+          (msg: Record<string, string | number>) => {
+            socket.to(userId).emit('removeItemFromCart', msg)
+          }
+        )
+      }
+    })
+
     this.instance.use(
       expressWinston.logger({
         transports: [new winston.transports.Console()],
