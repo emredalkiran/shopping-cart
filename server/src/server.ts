885627ac397304f27a5a647f226baf6c1331c0eb
emredalkiran <emre59dalkiran@gmail.com>
2021-02-25 23:17:15 +0300

    Socket added

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import http from 'http'
 import MongoConnection from './utils/database'
 import App from './app'
 import { creaeteServiceContainer } from './service-container'
+import { Server } from 'socket.io'
 
 async function init() {
   env.config()
@@ -17,11 +18,18 @@ async function init() {
     )
     console.log(`Connected to ${process.env.DATABASE_NAME} database `)
     console.log('Starting server...')
-    const serviceContainer = creaeteServiceContainer(db)
-    app.configureApp(serviceContainer)
     const server = http.createServer(app.instance)
-    server.listen({ host: 'localhost', port: port }, () =>
-      console.log(`Server listening on port ${port} at localhost`)
+    const io = new Server(server, {
+      cors: {
+        origin: 'http://127.0.0.1:3000',
+        methods: ['GET', 'POST'],
+        credentials: true
+      }
+    })
+    const serviceContainer = creaeteServiceContainer(db)
+    app.configureApp(serviceContainer, io)
+    server.listen({ host: '0.0.0.0', port: port }, () =>
+      console.log(`Server listening on port ${port}`)
     )
   } catch (err) {
     console.log(err)
